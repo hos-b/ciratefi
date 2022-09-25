6d9dc440871ef99f87582efed3c3601306ea86db
hos-b <hexed@outlook.com>
2022-09-25 09:45:14 +0200

    switch to at::Tensor, WIP

--- src/filters/circ_sample_filter.cpp
+++ src/filters/circ_sample_filter.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/core.hpp>
 #include <opencv2/core/hal/interface.h>
 #include <opencv2/imgproc.hpp>
+#include <torch/torch.h>
 
 #include <filters/circ_sample_filter.h>
 
@@ -64,55 +65,50 @@ static double sample_circle(cv::Mat image, int xc, int yc, int r)
  * @param raw_template tempalte image
  * @param scales scales
  * @param radi radii
- * @param polar_inter_flag interpolation flag for polar projection
  * @param resize_inter_flag interpolation flag for resizing
  * @return cv::Mat feature matrix
  */
-Eigen::Tensor<double, 2>
-compute_template_features(cv::Mat raw_template,
-                          const std::initializer_list<double> &scales,
-                          const std::initializer_list<int> &radi,
-                          int polar_inter_flag, int resize_inter_flag)
+at::Tensor compute_template_features(cv::Mat raw_template,
+                                     const std::vector<double> &scales,
+                                     const std::vector<int> &radii,
+                                     int resize_inter_flag)
 {
-    Eigen::Tensor<double, 2> cq(scales.size(), radi.size());
-    int scale_idx = 0;
-    for (auto scale : scales) {
+    at::Tensor cq = at::empty(
+        {static_cast<long>(scales.size()), static_cast<long>(radii.size())},
+        at::kDouble);
+    auto accessor = cq.accessor<double, 2>();
+    for (size_t s = 0; s < scales.size(); ++s) {
         cv::Mat resized_template;
-        if (scale == 1) {
+        if (scales[s] == 1) {
             resized_template = raw_template.clone();
         } else {
-            cv::resize(raw_template, resized_template, cv::Size(), scale, scale,
-                       resize_inter_flag);
+            cv::resize(raw_template, resized_template, cv::Size(), scales[s],
+                       scales[s], resize_inter_flag);
         }
-        // map to polar coordinates to make circle projection easier
-        cv::Mat resized_polar;
-        cv::linearPolar(
-            resized_template, resized_polar,
-            {resized_template.cols / 2.0f, resized_template.rows / 2.0f},
-            resized_template.cols / 2.0f, polar_inter_flag);
-        // reduce polar image along columns to a single row
-        cv::Mat col_features;
-        cv::reduce(resized_polar, col_features, 0, cv::REDUCE_AVG, CV_64F);
-        for (auto radius : radi) {
-            if (radius < resized_polar.cols) {
-                cq(scale_idx, radius) = col_features.at<double>(radius);
-            } else {
-                cq(scale_idx, radius) = 0.0;
-            }
+        int xc = resized_template.cols / 2;
+        int yc = resized_template.rows / 2;
+        // after this radius, circle falls completely outside the image
+        int max_radius = std::sqrt(xc * xc + yc * yc);
+        for (size_t r = 0; r < radii.size(); ++r) {
+            // circles
+            accessor[s][r] =
+                radii[r] <= max_radius
+                    ? sample_circle(resized_template, xc, yc, radii[r])
+                    : 0.0;
         }
-        scale_idx += 1;
     }
     return cq;
 }
 
-Eigen::Tensor<double, 3>
-compute_image_features(cv::Mat image, const std::initializer_list<int> &radi)
+at::Tensor compute_image_features(cv::Mat image, const std::vector<int> &radii)
 {
-    Eigen::Tensor<double, 3> circ_features(radi.size(), image.rows, image.cols);
-    for (auto r : radi) {
+    at::Tensor circ_features = at::empty(
+        {static_cast<long>(radii.size()), image.rows, image.cols}, at::kDouble);
+    auto accessor = circ_features.accessor<double, 3>();
+    for (size_t r = 0; r < radii.size(); ++r) {
         for (int i = 0; i < image.rows; ++i) {
             for (int j = 0; j < image.cols; ++j) {
-                circ_features(r, i, j) = sample_circle(image, j, i, r);
+                accessor[r][i][j] = sample_circle(image, j, i, radii[r]);
             }
         }
     }
@@ -126,61 +122,65 @@ compute_image_features(cv::Mat image, const std::initializer_list<int> &radi)
  * @param template_features per scale circular projection features [S, R]
  * @return max cross-corelation and the arg-max for scale detection
  */
-std::pair<Eigen::Tensor<double, 2>, Eigen::Tensor<long int, 2>>
-compute_circular_correlation(const Eigen::Tensor<double, 3> &image_features,
-                             const Eigen::Tensor<double, 2> &template_features)
+std::tuple<at::Tensor, at::Tensor>
+compute_circular_correlation(const at::Tensor &image_features,
+                             const at::Tensor &template_features)
 {
-    const int scale_count = template_features.dimension(0);
-    const int radius_count = image_features.dimension(0);
-    const int image_h = image_features.dimension(1);
-    const int image_w = image_features.dimension(2);
+    using namespace at::indexing;
+    const int scale_count = template_features.size(0);
+    const int radius_count = image_features.size(0);
+    const int image_h = image_features.size(1);
+    const int image_w = image_features.size(2);
     // cross correlation between the image and the template [H, W]
-    Eigen::Tensor<double, 2> correlation(image_h, image_w);
+    at::Tensor correlation = at::empty({image_h, image_w}, at::kDouble);
     // argmax for determining the scale [H, W]
-    Eigen::Tensor<int, 2> corr_argmax(image_h, image_w);
-    // calculate image mean & std across different radii
-    const Eigen::Tensor<double, 2> img_radius_mean =
-        image_features.mean(Eigen::array<int, 1>({0}));
-    Eigen::Tensor<double, 3> img_std_interm(radius_count, image_h, image_w);
+    at::Tensor corr_argmax = at::empty({image_h, image_w}, at::kInt);
+    // calculate image mean & std across different radii [H, W]
+    const at::Tensor img_radius_mean = image_features.mean({0});
+
+    // intermediate results: image_features - mean [R, H, W]
+    at::Tensor img_std_interm =
+        at::empty({radius_count, image_h, image_w}, at::kDouble);
     for (int r = 0; r < radius_count; r += 1) {
-        img_std_interm.chip(r, 0) =
-            (image_features.chip(r, 0) - img_radius_mean);
+        img_std_interm.index_put_(
+            {r, "..."}, (image_features.index({r, "..."}) - img_radius_mean));
     }
-    const Eigen::Tensor<double, 2> image_std =
-        img_std_interm.square().sum(Eigen::array<Eigen::Index, 1>({0})).sqrt();
+    const at::Tensor image_std = img_std_interm.square().sum({0}).sqrt();
 
     // calculate template std across different radii
-    const Eigen::Tensor<double, 1> tmp_radius_mean =
-        template_features.mean(Eigen::array<Eigen::Index, 1>({1}));
-    Eigen::Tensor<double, 2> tmp_std_interm(scale_count, radius_count);
+    at::Tensor tmp_radius_mean = template_features.mean({1});
+    at::Tensor tmp_std_interm =
+        at::empty({scale_count, radius_count}, at::kDouble);
     for (int s = 0; s < scale_count; s += 1) {
-        tmp_std_interm.chip(s, 0) =
-            (template_features.chip(s, 0) - tmp_radius_mean(s));
+        tmp_std_interm.index_put_(
+            {s, "..."},
+            (template_features.index({s, "..."}) - tmp_radius_mean.index({s})));
     }
-    const Eigen::Tensor<double, 1> template_std =
-        tmp_std_interm.square().sum(Eigen::array<int, 1>({0})).sqrt();
+    const at::Tensor template_std = tmp_std_interm.square().sum({0}).sqrt();
     // [S, H, W]
-    Eigen::Tensor<double, 3> cross_corr(scale_count, image_h, image_w);
+    at::Tensor cross_corr =
+        at::empty({scale_count, image_h, image_w}, at::kDouble);
+    auto t = torch::zeros({});
     for (int s = 0; s < scale_count; ++s) {
         for (int i = 0; i < image_h; ++i) {
             for (int j = 0; j < image_w; ++j) {
-                const Eigen::array<Eigen::Index, 3> offsets = {0, i, j};
-                const Eigen::array<Eigen::Index, 3> extents = {radius_count, 1,
-                                                               1};
-                const Eigen::array<Eigen::Index, 1> new_shape = {radius_count};
-                Eigen::Tensor<double, 0> numerator =
-                    (img_std_interm.slice(offsets, extents).reshape(new_shape) *
-                     tmp_std_interm.chip(s, 0))
-                        .sum();
-                cross_corr(s, i, j) =
-                    numerator(0) / (image_std(i, j) * template_std(s));
+                // const Eigen::array<Eigen::Index, 3> offsets = {0, i, j};
+                // const Eigen::array<Eigen::Index, 3> extents = {radius_count,
+                // 1,
+                //                                                1};
+                // const Eigen::array<Eigen::Index, 1> new_shape =
+                // {radius_count};
+                at::Scalar numerator = (img_std_interm.index({"...", i, j}) *
+                                        tmp_std_interm.index({s, "..."}))
+                                           .sum()
+                                           .item();
+                cross_corr.index_put_({s, i, j},
+                                      numerator / (image_std.index({i, j}) *
+                                                   template_std.index({s})));
             }
         }
     }
-    Eigen::Tensor<double, 2> corr =
-        cross_corr.maximum(Eigen::array<Eigen::Index, 1>({0}));
-    Eigen::Tensor<long int, 2> scale_idx = cross_corr.argmax(0);
-    return std::make_pair(corr, scale_idx);
+    return cross_corr.max(0);
 }
 
 } // namespace ciratefi::circle
--- src/main.cpp
+++ src/main.cpp
@@ -5,34 +5,14 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc.hpp>
 
+#define debug_trap __asm__ volatile("int3");
+
 int main(int argc, char ** argv)
 {
-    Eigen::Tensor<double, 3> tensor(3, 5, 5);
-    // clang-format: off
-    tensor.setValues({{{10, 0, 0, 0, 0},
-                       {0, 0, 0, 0, 0},
-                       {0, 0, 0, 0, 0},
-                       {0, 0, 0, 0, 0},
-                       {0, 0, 0, 0, 0}},
-                      {{10, 1, 1, 1, 1},
-                       {1, 1, 1, 1, 1},
-                       {1, 1, 1, 1, 1},
-                       {1, 1, 1, 1, 1},
-                       {1, 1, 1, 1, 1}},
-                      {{7, 2, 2, 2, 2},
-                       {2, 2, 2, 2, 2},
-                       {2, 2, 2, 2, 2},
-                       {2, 2, 2, 2, 2},
-                       {2, 2, 2, 2, 2}}});
     int chosen_scale = 0;
     if (argc > 1)
         chosen_scale = std::stoi(argv[1]);
-    // clang-format: on
-    // std::cout << tensor << std::endl;
-    // Eigen::Tensor<double, 2> mean = tensor.mean(Eigen::array<int, 1>({0}));
-    // std::cout << "mean =\n" << mean << std::endl;
-    // Eigen::Tensor<long int, 2> argmax = tensor.argmax(0);
-    // std::cout << "argmax =\n" << argmax << std::endl;
+
     cv::Mat image = cv::imread("/home/hosein/image.png", cv::IMREAD_GRAYSCALE);
     cv::Mat templ =
         cv::imread("/home/hosein/template.png", cv::IMREAD_GRAYSCALE);
@@ -47,7 +27,7 @@ int main(int argc, char ** argv)
     auto [corr, scale] =
         ciratefi::circle::compute_circular_correlation(img_f, tmp_f);
 
-    cv::Mat correlation(image.rows, image.cols, CV_64FC1, corr.data());
+    cv::Mat correlation(image.rows, image.cols, CV_64FC1, corr.data_ptr());
     cv::threshold(correlation, correlation, 0.0, 1.0, cv::THRESH_TOZERO);
 
     cv::Mat viz;
@@ -55,12 +35,13 @@ int main(int argc, char ** argv)
     for (int i = 0; i < viz.rows; ++i) {
         for (int j = 0; j < viz.cols; ++j) {
             double corr_num = correlation.at<double>(i, j);
-            if (scale(i, j) != chosen_scale || corr_num < 0.7)
+            if (scale.index({i, j}).item().to<int32_t>() != chosen_scale || corr_num < 0.7)
                 continue;
             viz.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 0, 255);
         }
     }
     cv::imshow("win", viz);
     cv::waitKey(0);
+    debug_trap
     return 0;
 }
