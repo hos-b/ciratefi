faf6766474c4c20253d9b88f9e3d1885f5efa5c7
hos-b <hexed@outlook.com>
2022-10-02 20:18:13 +0200

    remove unused headers

--- src/filters/circ_filter.cpp
+++ src/filters/circ_filter.cpp
@@ -1,8 +1,6 @@
 #include <opencv2/core.hpp>
-#include <opencv2/core/hal/interface.h>
 #include <opencv2/imgproc.hpp>
 
-#include <fmt/format.h>
 #include <torch/torch.h>
 
 namespace ciratefi::circle
