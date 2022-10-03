eba0dbbcb523857b31571b20fd3f1c09c80926ba
hos-b <hexed@outlook.com>
2022-10-03 11:05:31 +0200

    improve debug trap

--- include/debug_trap.h
+++ include/debug_trap.h
@@ -1,3 +1,7 @@
 #ifndef debug_trap
-#define debug_trap __asm__ volatile("int3")
+#include <iostream>
+#define debug_trap(x)                                                          \
+    std::cout << "\033[31mdebug trap @ " << __FILE__ << ":" << __LINE__       \
+              << ":\033[0m " << x << std::endl;                                \
+    __asm__ volatile("int3")
 #endif
